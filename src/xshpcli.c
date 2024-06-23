07294b46b3af44d06dfc2cab74fc99ccc9b4aa63
Dunno8489 <telefon.eryk99@gmail.com>
2024-06-23 09:22:01 +0200

    commit

---
+++ src/xshpcli.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char inptcli[] = "";
+    printf("XShop V. BETA-0.1\n");
+    printf("norder - Creates A New Order.\n");
+    printf("ilist - lists the current items in the order.\n");
+    printf("For More Info, see the documentation.\n");
+    printf("xshp>");
+    scanf("&s",inptcli);
+    return 0;
+}
